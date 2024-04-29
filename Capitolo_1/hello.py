a7c3e8b3f6c7961131661a8586d78995aa4ea3bf
Emanuele Nepa <nepaemanuele@outlook.it>
2024-04-29 19:49:50 +0200

    Examples from the book's first chapter

---
+++ Capitolo_1/hello.py
@@ -0,0 +1,9 @@
+#Il mio primo programma scritto in Python          # Commento
+
+print("Hello, World!")                              # Enunciato con stringa
+
+print(6*7)                                          # Valore numerico
+
+print("The result is", 6*7,"!")                     # Più argomenti
+
+ #print()                                            # Errore di indentazione
